35b6c1b91958b1f8468272211974886345ac8b58
Kaznov <kamkaz@outlook.com>
2021-12-03 15:44:12 +0100

    Change functor to binary flag in day3 solution

--- day3_part2.cpp
+++ day3_part2.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 using uint = unsigned;
-constexpr int kBinLen = 5; // length of binary numbers
+constexpr int kBinLen = 12; // length of binary numbers
 
 auto bin_to_num(const std::string& text_bin) -> uint {
     unsigned int num = 0;
@@ -14,9 +14,8 @@ auto bin_to_num(const std::string& text_bin) -> uint {
     return num;
 };
 
-template <typename SizeSelector>
 auto ratingFinder(const std::vector<uint>& input,
-                  SizeSelector&& selector) -> uint {
+                  bool is_co2) -> uint {
     // assert (is_sorted)
     auto it_lower = std::begin(input), it_upper = std::end(input);
     for (int i = 0; i < kBinLen && it_upper - it_lower > 1; ++i) {
@@ -26,7 +25,7 @@ auto ratingFinder(const std::vector<uint>& input,
         auto it_mid = std::lower_bound(it_lower, it_upper, separator);
         auto zeros_count = it_mid - it_lower;
         auto ones_count = it_upper - it_mid;
-        (selector(zeros_count, ones_count) ?
+        ((zeros_count <= ones_count) ^ is_co2 ?
             it_lower : it_upper) = it_mid;
     }
 
@@ -45,11 +44,8 @@ int main() {
 
     std::sort(std::begin(input_nums), std::end(input_nums));
 
-    std::sort(std::begin(input), std::end(input));
-    for (auto n : input) std::cout << n << "\n";
-
-    unsigned oxy_rating = ratingFinder(input_nums, std::less_equal<>{});
-    unsigned co2_rating = ratingFinder(input_nums, std::greater<>{});
+    unsigned oxy_rating = ratingFinder(input_nums, false);
+    unsigned co2_rating = ratingFinder(input_nums, true);
 
     std::cout << oxy_rating * co2_rating;
 }
