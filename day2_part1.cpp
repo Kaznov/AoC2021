946a68afdd9de42f768d80cb14ef2ad5c835a099
Kaznov <kamkaz@outlook.com>
2021-12-03 14:35:27 +0100

    Add day 2 C++ solutions

---
+++ day2_part1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string command;
+    int value;
+    int position = 0, depth = 0;
+    while (std::cin >> command >> value) {
+        if (command == "forward") {
+            position += value;
+        } else if (command == "down") {
+            depth += value;
+        } else {
+            depth -= value;
+        }
+    }
+    std::cout << position * depth;
+}
---
+++ day2_part2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string command;
+    int value;
+    long long int position = 0, depth = 0, aim = 0;
+    while (std::cin >> command >> value) {
+        if (command == "forward") {
+            position += value;
+            depth += value * aim;
+        } else if (command == "down") {
+            aim += value;
+        } else {
+            aim -= value;
+        }
+    }
+    std::cout << position * depth;
+}
