67d4b3a553bac02aeea6630b317bf9ad9ccf9a34
Kaznov <kamkaz@outlook.com>
2021-12-11 09:32:42 +0100

    Add day 10 C++ solutions

---
+++ day10_part1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main () {
+    std::string open = "([{<", close = ")]}>";
+    int values[] = {3, 57, 1197, 25137};
+    std::string line;
+    int err = 0;
+    while (std::getline(std::cin, line)) {
+        std::vector<char> s;
+        bool ok = true;
+        for (char c : line) {
+            if (open.contains(c)) s.push_back(c);
+            else if (s.size() > 0 && open.find(s.back()) == close.find(c)) s.pop_back();
+            else {
+                err += values[close.find(c)];
+                break;
+            }
+        }
+    }
+
+    std::cout << err;
+}
